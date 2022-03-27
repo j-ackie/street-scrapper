714b1ba557e64ffb9c44f348915d0c9b8cd142e0
Jackie Lin <jackielin2025@u.northwestern.edu>
2022-03-27 03:09:16 -0400

    revert

--- cmake-build-debug/.cs211/lib/catch/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/jackie/Projects/cs211-final-project/.cs211/lib/catch
-# Build directory: /Users/jackie/Projects/cs211-final-project/cmake-build-debug/.cs211/lib/catch
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- cmake-build-debug/CMakeFiles/game.dir/src/game_state.cxx.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/game.dir/src/game_state.cxx.o: \
-  /Users/jackie/Projects/cs211-final-project/src/game_state.cxx \
-  /Users/jackie/Projects/cs211-final-project/src/game_state.hxx
